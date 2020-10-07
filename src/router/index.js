e5b8ad6d7e4aab2c9b32aef6f66e1c0d3b55bf17
zgualberto <zngualberto@yahoo.com>
2020-10-07 23:14:46 +0800

    checkpoint

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,21 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import { IonicVueRouter } from "@ionic/vue";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(IonicVueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "home",
+    component: Home,
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+];
 
-const router = new VueRouter({
-  mode: 'history',
+const router = new IonicVueRouter({
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
